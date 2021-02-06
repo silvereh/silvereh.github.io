0e93bb1f9d298abaa888adbfc2085be7a1e64fd3
Silvere <silvere.heraudeau@gmail.com>
2021-02-06 13:32:42 -0700

    Updated Portfolio.js to actually read the feed

--- src/content/Portfolio.js
+++ src/content/Portfolio.js
@@ -37,24 +37,18 @@ export class Portfolio extends React.Component {
 				<div className="Portfolio-content">
 					<h2>My Projects</h2>
 					<div id="Portfolio-articles">
-						{/*
+						{
 							this.state.items.map(item => {
 								return (
 									<a className="Portfolio-article" href={item.link}>
 										<h3 className="Portfolio-article-title">{item.title}</h3>
 										<p className="Portfolio-article-tech">{item.categories}</p>
-										<img className="Portfolio-article-thumb img-responsive" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" />
+										<img className="Portfolio-article-thumb img-responsive" src={item.thumbnail} />
 										<p className="Portfolio-article-summary">{item.contentSnippet}</p>
 									</a>
 								);
 							})
-						*/}
-						<a className="Portfolio-article" href="{item.link}">
-							<h3 className="Portfolio-article-title">Silvereh.github.io</h3>
-							<p className="Portfolio-article-tech">CSS, HTML, JavaScript, jekyll, Liquid, React, SCSS</p>
-							<img className="Portfolio-article-thumb img-responsive" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" />
-							<p className="Portfolio-article-summary">Lorem ipsum dolor sit, amet consectetur adipisicing, elit. Ad exercitationem quae adipisci, praesentium optio numquam unde placeat tenetur, nostrum excepturi rem recusandae reiciendis. Inventore quia fugit odit neque commodi blanditiis.</p>
-						</a>
+						}
 					</div>
 				</div>
 			</section>
